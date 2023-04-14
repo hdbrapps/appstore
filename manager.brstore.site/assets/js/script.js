0570a1345be1e17a055315430945faf75a2d28c2
vanrley dev <99355912+hdbrapps@users.noreply.github.com>
2023-04-14 10:56:58 -0300

    commit in

---
+++ manager.brstore.site/assets/js/script.js
@@ -0,0 +1,63 @@
+const codeInput = document.getElementById('code-input');
+const submitButton = document.getElementById('submit-button');
+const errorMessage = document.getElementById('error-message');
+const successMessage = document.getElementById('success-message');
+
+// Função para redirecionar para a página de login após 1 minuto
+function redirectBackToLogin() {
+  localStorage.removeItem('lastVisit');
+  window.location.href = 'index.html';
+}
+
+// Verifica se a senha foi salva anteriormente
+const savedCode = localStorage.getItem('savedCode');
+if (savedCode) {
+  codeInput.value = savedCode;
+}
+
+// Trata a submissão do formulário
+function handleSubmit() {
+  const code = codeInput.value.trim();
+  if (code === '') {
+    errorMessage.innerText = 'Digite o código';
+    return;
+  }
+  if (code !== 'cavalodetroia') {
+    errorMessage.innerText = 'Código inválido';
+    return;
+  }
+
+  // Salva a senha digitada pelo usuário
+  localStorage.setItem('savedCode', code);
+
+  // Limpa as mensagens de erro e sucesso
+  errorMessage.innerText = '';
+  successMessage.classList.remove('hidden');
+
+  // Armazena a hora em que o usuário acessou a página principal
+  localStorage.setItem('lastVisit', new Date().getTime());
+
+  // Redireciona para a página principal
+  window.location.href = 'site.html';
+}
+
+// Trata o clique no botão de submissão
+submitButton.addEventListener('click', handleSubmit);
+
+// Trata a tecla Enter no campo de senha
+codeInput.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
+    handleSubmit();
+  }
+});
+
+// Verifica se o usuário deve ser redirecionado para a página de login
+const lastVisit = localStorage.getItem('lastVisit');
+if (lastVisit) {
+  const timeElapsed = new Date().getTime() - lastVisit;
+  if (timeElapsed > 60000) {
+    redirectBackToLogin();
+  } else {
+    setTimeout(redirectBackToLogin, 60000 - timeElapsed);
+  }
+}
